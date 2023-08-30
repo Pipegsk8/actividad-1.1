f523f54d357196558a9ebc740c29f3a0e24e3872
Pipegsk8 <143115717+Pipegsk8@users.noreply.github.com>
2023-08-30 17:36:29 -0400

    a
    a

---
+++ app/src/main/java/com/felipe/actividad/MainActivity.java
@@ -0,0 +1,20 @@
+package com.felipe.actividad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+
+    }
+
+}
